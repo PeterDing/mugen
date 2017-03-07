bb11956e0cada461166fc16775fd5685b6c3fa08
PeterDing <dfhayst@gmail.com>
2017-03-07 14:51:41 +0800

    [adapters.py] rules of creating connection are here

--- mugen/adapters.py
+++ mugen/adapters.py
@@ -5,6 +5,12 @@ from __future__ import unicode_literals, absolute_import
 import logging
 import asyncio
 
+from mugen.utils import is_ip, parse_proxy
+from mugen.exceptions import UnknownProxyScheme
+from mugen.proxy import (
+    _make_https_proxy_connection,
+    Socks5Proxy
+)
 from mugen.models import (
     Singleton,
     Response,
@@ -25,8 +31,73 @@ class HTTPAdapter(Singleton):
         self._initiated = True
         self.recycle = recycle
         self.loop = loop or asyncio.get_event_loop()
-        # self.connection_pool = ConnectionPool(recycle=recycle, loop=self.loop)
         self.connection_pool = connection_pool
+
+
+    @asyncio.coroutine
+    def generate_direct_connect(self, host, port, ssl, dns_cache, recycle=True):
+        key = None
+        if is_ip(host):
+            ip = host.split(':')[0]
+            key = (ip, port, ssl)
+
+        if not key and not ssl:
+            ip, port = yield from dns_cache.get(host, port)
+            key = (ip, port, ssl)
+
+        if not key and ssl:
+            key = (host, port, ssl)
+
+        conn = yield from self.get_connection(key, recycle=recycle)
+        return conn
+
+
+    @asyncio.coroutine
+    def generate_proxy_connect(self, host, port, ssl, proxy, dns_cache, recycle=True):
+        proxy_scheme, proxy_host, proxy_port, username, password = parse_proxy(proxy)
+
+        proxy_ip, proxy_port = yield from dns_cache.get(proxy_host, proxy_port)
+        if ssl:
+            key = (proxy_ip, proxy_port, False, host)
+        else:
+            key = (proxy_ip, proxy_port, False)
+
+        if proxy_scheme.lower() == 'http':
+            conn = yield from self.generate_http_proxy_connect(
+                key, host, port, ssl, username, password, recycle=recycle)
+        elif proxy_scheme.lower() == 'socks5':
+            conn = yield from self.generate_socks5_proxy_connect(
+                key, host, port, ssl, username, password, recycle=recycle)
+        else:
+            raise UnknownProxyScheme(proxy_scheme)
+        return conn
+
+
+    @asyncio.coroutine
+    def generate_http_proxy_connect(
+        self, key, host, port, ssl, username, password, recycle=True):
+
+        conn = yield from self.get_connection(key, recycle=recycle)
+
+        if ssl and not conn.ssl_on:
+            logging.debug('[ssl_handshake]: {}'.format(key))
+            yield from _make_https_proxy_connection(
+                conn, host, port, recycle=recycle)
+            conn.ssl_on = True
+        return conn
+
+
+    def generate_socks5_proxy_connect(
+        self, key, host, port, ssl, username, password, recycle=True):
+
+        conn = yield from self.get_connection(key, recycle=recycle)
+        if conn.socks_on:
+            return conn
+
+        socks5_proxy = Socks5Proxy(conn, host, port, ssl, username, password)
+        yield from socks5_proxy.init()
+        conn.socks_on = True
+        return conn
 
 
     @asyncio.coroutine
