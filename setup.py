9fd6f2d55ff6793468e17d056610b6774f2a18d0
PeterDing <dfhayst@gmail.com>
2021-07-14 15:49:44 +0800

    Let poetry control

--- setup.py
+++ setup.py
@@ -1,35 +1,8 @@
-import sys
+#!/usr/bin/env python
 
+# This is a shim to hopefully allow Github to detect the package, build is done with poetry
 
-try:
-    from setuptools import setup
-except ImportError:
-    # Use distutils.core as a fallback.
-    # We won't be able to build the Wheel file on Windows.
-    from distutils.core import setup
+import setuptools
 
-if sys.version_info < (3, 4, 0):
-    raise RuntimeError("mugen requires Python 3.4.0+")
-
-version = "0.4.4"
-
-requires = ["httptools"]
-
-setup(
-    name="mugen",
-    version=version,
-    author="PeterDing",
-    author_email="dfhasyt@gmail.com",
-    license="Apache 2.0",
-    description="Mugen is library for http asynchronous requests",
-    url="http://github.com/PeterDing/mugen",
-    install_requires=requires,
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-    ],
-    packages=["mugen"],
-)
+if __name__ == "__main__":
+    setuptools.setup(name="mugen")
